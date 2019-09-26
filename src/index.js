7593ccfb373cfc22a749cc7842e7567d50fc5373
Talha Saleem <talha.saleem@techlogix.com>
2019-09-26 18:28:22 +0500

    Added a clock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <div>welcome to this course</div>
-  , document.querySelector('#root'));
+    class Clock extends React.Component {
+       constructor(props){
+           super(props);
+           this.state = {timer: new Date()};
+       }
+       componentDidMount() {
+        this.timerID = setInterval(
+          () => this.updateState(),
+          1000
+        );
+      }
+      componentWillUnmount() {
+        clearInterval(this.timerID);
+      }
+
+       updateState(){
+            this.setState({timer:new Date()});
+       }
+
+    
+        render() {
+            return  <p>Current time:<h2> {this.state.timer.toLocaleTimeString()} </h2></p>;
+            }
+        }
+        
+        ReactDOM.render( <Clock/>, document.querySelector('#root'));
+      
